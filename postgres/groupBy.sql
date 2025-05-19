6d5eb016ac756adc01c139d54f0cea811216ae13
rubelrana123 <rubelranatpi707458@gmail.com>
2025-05-19 23:56:46 +0600

    learn group by

---
+++ postgres/groupBy.sql
@@ -0,0 +1,10 @@
+SELECT country, count(*) FROM students
+GROUP BY country;
+
+SELECT count(country) as total,country, avg(age)  FROM students
+GROUP BY country
+HAVING NOT avg(age) = 21.0000000000000000;
+
+
+SELECT  extract(year FROM dob) as birth_year , count(birth_year) from students 
+GROUP BY birth_year
